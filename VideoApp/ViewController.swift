301c6cd4c5496bbfac2b4721ce11cf5482b005c9
demothreen <demothreen@intersvyaz.net>
2019-04-26 16:24:54 +0500

    Initial Commit

---
+++ VideoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VideoApp
+//
+//  Created by Marina on 26/04/2019.
+//  Copyright © 2019 demothreen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
